f2e01a04aa04fd74befb021aeb7b52421d31bb3d
Yurii Mykhailiv <59884970+yurii-mykhailiv@users.noreply.github.com>
2020-06-22 18:36:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', function () {
+    let products = document.querySelectorAll(".product"),
+      buttons = document.querySelectorAll("button"),
+      openBtn = document.querySelector(".open");
+   
+    function createCart() {
+        let cart = document.createElement('div'),
+            field = document.createElement('div'),
+            heading = document.createElement('h2'),
+            closeBtn = document.createElement('button');
+        cart.classList.add('cart');
+        field.classList.add('cart-field');
+        closeBtn.classList.add('close');
+
+        heading.textContent = " В нашому кошику:";
+        closeBtn.textContent = "Закрити";
+
+        document.body.appendChild(cart);
+        cart.appendChild(heading);
+        cart.appendChild(field);
+        cart.appendChild(closeBtn);
+    }
+    createCart();
+
+    let field = document.querySelector('.cart-field'),
+        cart = document.querySelector('.cart'),
+        close = document.querySelector('.close');
+    
+    function openCart() {
+        cart.style.display = 'block';
+    }
+     function closeCart() {
+       cart.style.display = "none";
+    }
+    
+    openBtn.addEventListener('click', openCart);
+    close.addEventListener("click", closeCart);
+
+   
+
+    buttons.forEach((item, i) => {
+         
+        item.addEventListener('click',  () =>  {
+            let item = products[i].cloneNode(true),
+                btn = item.querySelector('button');
+            
+            btn.remove();
+            field.appendChild(item);
+            products[i].remove();
+        });
+    });
+   
+});
+
+
+
